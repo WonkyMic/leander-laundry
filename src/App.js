c43024116adff3e35d482900028a9c4e612a36da
mic <wonkymic@gmail.com>
2017-08-26 16:23:43 -0500

    refactor: Default app to Leander Laundry

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './ll-logo.png';
 import './App.css';
 
 class App extends Component {
+  clickHome() {
+    alert('Clicked Home');
+    //<a href='/Hello.js'> Click me? </a>
+    //ReactDOM.render(<Home />, document.getElementById('root'))
+    //window.location = './Home.js';
+  }
+  clickWashClub() {
+    alert('Clicked Wash Club');
+  }
+
+  header() {
+    return(
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" align="right" />
+        <button onClick={this.clickHome}>
+          Home
+        </button>
+        <button onClick={this.clickWashClub}>
+          Discount Car Wash Club
+        </button>
+      </div>
+    );
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
+        {this.header()}
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Our Services.
         </p>
       </div>
     );
