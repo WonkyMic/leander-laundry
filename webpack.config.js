65650abe2073051cd39a7da5e31541df3f5f31e6
mic <wonkymic@gmail.com>
2017-08-27 00:37:31 -0500

    chore: added webpack structures

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var webpack = require('webpack');
+var pack = require('path');
+
+const BUILD_DIR = path.resolve(__dirname, './public');
+const APP_DIR = path.resolve(__dirname, './src');
+
+var config = {
+  entry: APP_DIR + '/index.js',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  }
+};
+
+module.exports = config;
